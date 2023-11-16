0cb51d36a936aa801f8215fa13dacfc383d5ce10
BomsG <120387032+BomsG@users.noreply.github.com>
2023-11-16 22:32:57 +0100

    Revert "Created the Signup, Set new password, and reset successful pages"

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,12 @@
-import React from 'react'
-import SignUp from './components/SignUp';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import SetNewPassword from './components/SetNewPassword';
-import ResetSuccessful from './components/ResetSuccessful';
-import Posts from './components/Posts';
+import { useState } from "react";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className='App'>
-        <Routes>
-          <Route path='/signup' element={<SignUp/>} />
-          <Route path='/set-new-password' element={<SetNewPassword/>} />
-          <Route path='/reset-successful' element={<ResetSuccessful/>} />
-          <Route path='/posts' element={<Posts/>} />
-        </Routes>
+    <>
+      <div>
+        <h1 font-bold>hello</h1>
       </div>
-
-    </BrowserRouter>
+    </>
   );
 }
 
